9d2befe60e56781507630cb5d49d6f3ac076f222
Jonas Bjøralt <jbb@variant.no>
2023-09-08 10:01:48 +0200

    :skull: :tada: api is dead, long live backend

--- api/Shared/AppSettings.cs
+++
@@ -1,9 +0,0 @@
-namespace Shared;
-
-public record AppSettings
-{
-    public Uri AzureAppConfigUri { get; set; } = null!;
-    public bool UseAzureAppConfig { get; set; }
-    
-    public string TenantId { get; set; }
-}
