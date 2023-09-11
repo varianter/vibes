0f57cf3e4dd7252be9d4799192eae8aa616f13e0
Jonas Bjøralt <jbb@variant.no>
2023-09-11 11:29:34 +0200

    working implementation (except for eslint)

--- frontend/src/app/weather.tsx
+++ frontend/src/app/weather.tsx
@@ -1,30 +1,31 @@
 "use client";
 
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 import { useIsAuthenticated, useMsal } from "@azure/msal-react";
-import { fetchWithToken } from "@/utils/ApiUtils";
+import { fetchWithToken } from "../utils/ApiUtils";
 
 export function WeatherForecast() {
-    const [data, setData] = useState<{temperatureC: number}>({temperatureC: 0})
-    const [isLoading, setLoading] = useState(true)
-    const isAuthenticated = useIsAuthenticated();
+  const [data, setData] = useState<{ temperatureC: number }>({
+    temperatureC: 0,
+  });
+  const [isLoading, setLoading] = useState(true);
+  const isAuthenticated = useIsAuthenticated();
 
-    useEffect(() => {
-      if(isAuthenticated){
-        fetchWithToken('/api/WeatherForecast')
-            .then((data) => {
-                setData(data)
-                setLoading(false)
-            })
-      }
-    }, [isAuthenticated])
+  useEffect(() => {
+    if (isAuthenticated) {
+      fetchWithToken("/api/WeatherForecast").then((data) => {
+        setData(data);
+        setLoading(false);
+      });
+    }
+  }, [isAuthenticated]);
 
-    if (isLoading) return <p>Loading...</p>
-    if (!data) return <p>No profile data</p>
+  if (isLoading) return <p>Loading...</p>;
+  if (!data) return <p>No profile data</p>;
 
-    return (
-        <div>
-            <h1>{data.temperatureC}</h1>
-        </div>
-    )
+  return (
+    <div>
+      <h1>{data.temperatureC}</h1>
+    </div>
+  );
 }
