fd2f8f220eb4674c5189237e000f90d1a800dce4
Jonas Bjøralt <jbb@variant.no>
2023-09-11 09:50:08 +0200

    add prettier, apply formatting

--- frontend/src/app/weather.tsx
+++ frontend/src/app/weather.tsx
@@ -1,26 +1,28 @@
 "use client";
 
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 
 export function WeatherForecast() {
-    const [data, setData] = useState<{temperatureC: number}>({temperatureC: 0})
-    const [isLoading, setLoading] = useState(true)
+  const [data, setData] = useState<{ temperatureC: number }>({
+    temperatureC: 0,
+  });
+  const [isLoading, setLoading] = useState(true);
 
-    useEffect(() => {
-        fetch('/api/WeatherForecast')
-            .then((res) => res.json())
-            .then((data) => {
-                setData(data)
-                setLoading(false)
-            })
-    }, [])
+  useEffect(() => {
+    fetch("/api/WeatherForecast")
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+        setLoading(false);
+      });
+  }, []);
 
-    if (isLoading) return <p>Loading...</p>
-    if (!data) return <p>No profile data</p>
+  if (isLoading) return <p>Loading...</p>;
+  if (!data) return <p>No profile data</p>;
 
-    return (
-        <div>
-            <h1>{data.temperatureC}</h1>
-        </div>
-    )
+  return (
+    <div>
+      <h1>{data.temperatureC}</h1>
+    </div>
+  );
 }
