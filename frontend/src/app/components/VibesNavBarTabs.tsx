d3dc2ba7edbc9f37a5e9d53351832a02289b81de
Magnus Gule <mg@variant.no>
2023-09-20 13:50:19 +0200

    Refactor VibesNavBar

---
+++ frontend/src/app/components/VibesNavBarTabs.tsx
@@ -0,0 +1,24 @@
+'use client';
+import { Tabs, TabsProps, styled } from "@mui/material";
+import React from "react";
+
+
+export const VibesNavBarTabs = styled((props: VibesNavBarTabsProps) => (
+    <Tabs
+        {...props}
+        TabIndicatorProps={{ children: <span className="MuiTabs-indicatorSpan" /> }} />
+))({
+    '& .MuiTabs-indicator': {
+        display: 'flex',
+        justifyContent: 'center',
+        backgroundColor: 'transparent',
+    },
+    '& .MuiTabs-indicatorSpan': {
+        width: '100%',
+        backgroundColor: 'rgba(104, 233, 221, 1)',
+    },
+});
+
+interface VibesNavBarTabsProps extends TabsProps {
+    children?: React.ReactNode;
+}
