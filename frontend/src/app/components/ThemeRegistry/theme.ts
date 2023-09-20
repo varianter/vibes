cd45a9b1b5262a5273f46ed8f85cf1f4c04b1647
Magnus Gule <mg@variant.no>
2023-09-20 13:05:23 +0200

    Removed unused roboto

--- frontend/src/app/components/ThemeRegistry/theme.ts
+++ frontend/src/app/components/ThemeRegistry/theme.ts
@@ -1,11 +1,4 @@
 import { createTheme } from '@mui/material/styles';
-import { Roboto } from 'next/font/google';
-
-const roboto = Roboto({
-  weight: ['300', '400', '500', '700'],
-  subsets: ['latin'],
-  display: 'swap',
-});
 
 const theme = createTheme({
   typography: {
