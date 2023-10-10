fc51b48fdf45516c43f90aad1fc2e1c3e0fe5857
Sigrid <55406589+sigtheidiot@users.noreply.github.com>
2023-10-10 13:48:43 +0200

    Style variant list (#114)
    * Style variant list
    
    * move border

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,25 @@
-import { VariantList } from "../components/variants";
+"use client";
+import VariantListElement from "@/components/variantListElement";
+import useVibesApi from "@/hooks/useVibesApi";
+import { CircularProgress } from "@mui/material";
 
-export default function Page() {
+export default function Bemanning() {
+  const { data, isLoading } = useVibesApi(true);
 
-    return <VariantList />
+  if (isLoading) {
+    return <CircularProgress />;
+  }
+
+  if (data) {
+    return (
+      <div>
+        <h1>Konsulenter</h1>
+
+        <p className="body-large-bold pt-16 pb-4">Konsulentliste</p>
+        {data.map((variant) => (
+          <VariantListElement key={variant.id} variant={variant} />
+        ))}
+      </div>
+    );
+  }
 }
