69494179347a3369cae080860da7f9760b3bb02e
Mathilde Haukø Haugum <mathilde_haugum@hotmail.com>
2023-10-11 08:02:39 +0200

    Add min height to side bar

--- frontend/src/app/bemanning/layout.tsx
+++ frontend/src/app/bemanning/layout.tsx
@@ -1,9 +1,9 @@
-import SearchBar from "@/components/searchBar";
+import SearchBar from "@/components/SearchBar";
 
 const BemanningLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex flex-row">
-      <div className="bg-primary_l4 py-6 px-4 flex flex-col gap-6">
+      <div className="bg-primary_l4 py-6 px-4 flex flex-col gap-6 min-h-screen">
         <h3 className="">Filter</h3>
         <SearchBar />
       </div>
