dcaef93da06423765bbba918e74565129df7f144
Sigrid Elnan <sge@variant.no>
2023-10-17 11:21:43 +0200

    Add actuall button design

--- frontend/src/components/NavBarDropdown.tsx
+++ frontend/src/components/NavBarDropdown.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useState } from "react";
 import { signOut } from "next-auth/react";
-import { LogOut } from "react-feather";
+import { LogOut, User } from "react-feather";
 
 export default function Dropdown(props: { initials: string }) {
   const [isOpen, setIsOpen] = useState<boolean>(false);
@@ -22,13 +22,17 @@ export default function Dropdown(props: { initials: string }) {
           <p className="text-white body-small">{props.initials}</p>
         </button>
         <div
-          className={`absolute origin-right right-0 bg-primary_default text-white flex flex-col w-[138px] border border-primary_l1 rounded-sm ${transClass}`}
+          className={`absolute origin-right right-0 top-11 rounded text-primary_default bg-white flex flex-col w-[138px] shadow-[0_4px_4px_0_rgba(66,61,137,0.10)]  ${transClass}`}
         >
+          <button className="text-center p-2 w-[138px] rounded flex flex-row gap-3 align-middle hover:bg-primary_default hover:text-white">
+            <User className=" w-4 h-4" />
+            <p className="body">Min profil</p>
+          </button>
           <button
-            className="text-center p-2 w-[138px] flex flex-row gap-3 align-middle hover:opacity-70"
+            className="text-center p-2 w-[138px] rounded flex flex-row gap-3 align-middle hover:bg-primary_default hover:text-white"
             onClick={() => signOut()}
           >
-            <LogOut className="text-white w-4 h-4" />
+            <LogOut className=" w-4 h-4" />
             <p className="body">Logg ut</p>
           </button>
         </div>
