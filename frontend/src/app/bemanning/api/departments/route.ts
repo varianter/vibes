9687e69b3d45359a413ca504394ba82e46a26e8b
Mathilde Haukø Haugum <mathilde_haugum@hotmail.com>
2023-10-19 15:14:28 +0200

    Move route handler

--- frontend/src/app/bemanning/api/departments/route.ts
+++
@@ -1,8 +0,0 @@
-import { fetchWithToken } from "@/data/fetchWithToken";
-import { Department } from "@/types";
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  const departments = (await fetchWithToken<Department[]>("departments")) ?? [];
-  return NextResponse.json(departments);
-}
