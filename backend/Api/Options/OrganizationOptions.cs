92f5b66565d8abfa05f4eb87627388c1f991a810
Magnus Gule <mg@variant.no>
2023-10-20 13:36:03 +0200

    * Moved HolidayService to be Consultant dependant. (#165)

--- backend/Api/Options/OrganizationOptions.cs
+++
@@ -1,9 +0,0 @@
-namespace Api.Options;
-
-public class OrganizationOptions
-{
-    public double HoursPerWorkday { get; set; }
-    public bool HasVacationInChristmas { get; set; }
-    public int NumberOfVacationDaysInYear { get; set; }
-    public string Country { get; set; }
-}
