ac6180cb2215a24d1584af6d759472a627ad8e02
Jonas Bjøralt <jbb@variant.no>
2023-10-24 09:35:18 +0200

    Add years of exp in readmodel

--- backend/Api/Consultants/ConsultantReadModel.cs
+++ backend/Api/Consultants/ConsultantReadModel.cs
@@ -3,4 +3,5 @@ using Core.DomainModels;
 namespace Api.Consultants;
 
 public record ConsultantReadModel(int Id, string Name, string Email, List<string> Competences, string Department,
+    int YearsOfExperience, Degree Degree,
     List<BookedHoursPerWeek> Bookings, bool IsOccupied);
