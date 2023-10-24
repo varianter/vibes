cfbf87c6bcb026fbc077754cf7c41addaed23a58
Jonas Bjøralt <61310258+jonasbjoralt@users.noreply.github.com>
2023-10-24 12:47:41 +0200

    172 exp filter rebased (#175)
    Co-authored-by: Sigrid Elnan <sge@variant.no>

---
+++ frontend/src/components/ExperienceFilter.tsx
@@ -0,0 +1,38 @@
+"use client";
+import { YearRange } from "@/types";
+import FilterButton from "./FilterButton";
+import { useFilteredConsultants } from "@/hooks/useFilteredConsultants";
+
+export const yearRanges: YearRange[] = [
+  { label: "0-2", urlString: "0-2", start: 0, end: 2 },
+  { label: "3-4", urlString: "3-4", start: 3, end: 4 },
+  { label: "5-7", urlString: "5-7", start: 5, end: 7 },
+  { label: "8-11", urlString: "8-11", start: 8, end: 11 },
+  { label: "12+", urlString: "12", start: 12 },
+];
+
+export default function ExperienceFilter() {
+  const { filteredYears, toggleYearFilter } = useFilteredConsultants();
+
+  if (yearRanges.length > 0) {
+    return (
+      <div>
+        <div className="flex flex-col gap-2">
+          <p className="body-small">Erfaring</p>
+          <div className="flex flex-col gap-2 w-52">
+            {yearRanges?.map((range, index) => (
+              <FilterButton
+                key={range.label}
+                label={range.label}
+                onClick={() => toggleYearFilter(range)}
+                checked={filteredYears
+                  .map((y) => y.urlString)
+                  .includes(range.urlString)}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
