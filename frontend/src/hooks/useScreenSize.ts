3de1864c3e8005c5ca23453639caa1c3f4269717
Magnus Gule <mg@variant.no>
2023-10-26 14:07:13 +0200

    Improved code readability

--- frontend/src/hooks/useScreenSize.ts
+++ frontend/src/hooks/useScreenSize.ts
@@ -6,31 +6,32 @@ interface ScreenSize {
   height: number;
 }
 
-function windowIsUp() {
-  return typeof window !== "undefined";
-}
-
 function useScreenSize(): ScreenSize {
-  const [screenSize, setScreenSize] = useState<ScreenSize>({
-    width: (windowIsUp() && window.innerWidth) || 0,
-    height: (windowIsUp() && window.innerHeight) || 0,
-  });
+  const isClient = typeof window === "object";
+
+  const [screenSize, setScreenSize] = useState<ScreenSize>(() => ({
+    width: isClient ? window.innerWidth : 0,
+    height: isClient ? window.innerHeight : 0,
+  }));
 
   useEffect(() => {
+    if (!isClient) {
+      return;
+    }
+
     function handleResize() {
       setScreenSize({
-        width: window.innerWidth ?? 0,
-        height: window.innerHeight ?? 0,
+        width: window.innerWidth,
+        height: window.innerHeight,
       });
     }
 
     window.addEventListener("resize", handleResize);
 
-    // Clean up the event listener when the component unmounts
     return () => {
       window.removeEventListener("resize", handleResize);
     };
-  }, []);
+  }, [isClient]);
 
   return screenSize;
 }
