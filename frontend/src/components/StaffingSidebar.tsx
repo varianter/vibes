5fdc255b4475bfe83a7cc7091819758f37169ecb
Mathilde Haukø Haugum <mathilde_haugum@hotmail.com>
2023-10-26 10:00:50 +0200

    Add fixed to top div

--- frontend/src/components/StaffingSidebar.tsx
+++ frontend/src/components/StaffingSidebar.tsx
@@ -9,10 +9,12 @@ export default function StaffingSidebar() {
   const [isSidebarHidden, setIsSidebarHidden] = useState(true);
 
   return (
-    <div>
+    <div className="min-h-screen fixed">
       <div
         className={`bg-primary_l4 py-6 px-4 h-full min-h-screen w-[211px]  ${
-          isSidebarHidden ? "absolute -left-[211px]" : "sticky top-[52px]"
+          isSidebarHidden
+            ? "absolute -left-[211px]"
+            : "fixed sm:sticky top-[52px]"
         }`}
       >
         <div className="flex flex-col gap-6 fixed w-[179px]">
@@ -33,7 +35,7 @@ export default function StaffingSidebar() {
       <button
         onClick={() => setIsSidebarHidden(false)}
         className={`bg-primary_l3 rounded-r p-2 mt-6 hover:bg-primary_default hover:bg-opacity-20 ${
-          !isSidebarHidden ? "hidden" : "sticky top-[76px]"
+          isSidebarHidden ? "top-[200px]" : "hidden"
         }`}
       >
         <Filter className="text-primary_default" size="20" />
