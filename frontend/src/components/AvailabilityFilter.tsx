98940fd9cbc2b721652e9a1dc57768d324c75205
Mathilde Haukø Haugum <mathilde_haugum@hotmail.com>
2023-11-02 14:01:08 +0100

    Implement filtering on availability

---
+++ frontend/src/components/AvailabilityFilter.tsx
@@ -0,0 +1,23 @@
+"use client";
+import FilterButton from "./FilterButton";
+import { useFilteredConsultants } from "@/hooks/useFilteredConsultants";
+
+export default function AvailabilityFilter() {
+  const { availabilityFilterOn, toggleAvailabilityFilter } =
+    useFilteredConsultants();
+
+  return (
+    <div>
+      <div className="flex flex-col gap-2">
+        <p className="body-small">Status</p>
+        <div className="flex flex-col gap-2 w-52">
+          <FilterButton
+            label={"Ledig tid"}
+            onClick={() => toggleAvailabilityFilter(!availabilityFilterOn)}
+            checked={availabilityFilterOn}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
