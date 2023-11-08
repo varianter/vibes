cd5fa15382b16756ce932a5475aea84aab24cfb2
Jonas Bjøralt <61310258+jonasbjoralt@users.noreply.github.com>
2023-11-08 12:58:56 +0100

    Add hotkey binding (#264)

--- frontend/src/hooks/staffing/useAvailabilityFilter.ts
+++ frontend/src/hooks/staffing/useAvailabilityFilter.ts
@@ -1,20 +1,38 @@
 "use client";
 
-import { useCallback } from "react";
+import { useCallback, useEffect } from "react";
 import { useUrlRouteFilter } from "./useUrlRouteFilter";
 
 export function useAvailabilityFilter() {
   const { updateRoute } = useUrlRouteFilter();
   const { availabilityFilter } = useUrlRouteFilter();
+  const availabilityFilterOn = availabilityFilter === "true";
 
-  const toggleAvailabilityFilter = useCallback(
+  const setAvailabilityFilter = useCallback(
     (availabelFilterOn: Boolean) => {
       updateRoute({ availability: availabelFilterOn });
     },
     [updateRoute],
   );
 
-  const availabilityFilterOn = availabilityFilter === "true";
+  const toggleAvailabilityFilter = useCallback(
+    () => setAvailabilityFilter(!availabilityFilterOn),
+    [availabilityFilterOn, setAvailabilityFilter],
+  );
+
+  useEffect(() => {
+    function keyDownHandler(e: { code: string }) {
+      if (e.code.includes("Period")) {
+        toggleAvailabilityFilter();
+      }
+    }
+    document.addEventListener("keydown", keyDownHandler);
+
+    // clean up
+    return () => {
+      document.removeEventListener("keydown", keyDownHandler);
+    };
+  }, [toggleAvailabilityFilter]);
 
   return {
     availabilityFilterOn,
