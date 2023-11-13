c3d1e468f9a817b5d7d8eda71ad2acdf851e7412
Sigrid <55406589+sigtheidiot@users.noreply.github.com>
2023-11-13 12:45:28 +0100

    Make cells equal again (#277)

--- frontend/src/components/FilteredConsultantsList.tsx
+++ frontend/src/components/FilteredConsultantsList.tsx
@@ -4,9 +4,12 @@ import ActiveFilters from "./ActiveFilters";
 import WeekSelection from "@/components/WeekSelection";
 import { isCurrentWeek } from "@/hooks/staffing/dateTools";
 import { useConsultantsFilter } from "@/hooks/staffing/useConsultantsFilter";
+import { useUrlRouteFilter } from "@/hooks/staffing/useUrlRouteFilter";
 
 export default function FilteredConsultantList() {
   const { filteredConsultants } = useConsultantsFilter();
+
+  const { weekSpan } = useUrlRouteFilter();
 
   return (
     <div className="flex flex-col gap-8">
@@ -14,7 +17,15 @@ export default function FilteredConsultantList() {
         <ActiveFilters />
         <WeekSelection />
       </div>
-      <table className="min-w-full table-fixed">
+      <table
+        className={`w-full ${
+          weekSpan > 23
+            ? "min-w-[1400px]"
+            : weekSpan > 11
+            ? "min-w-[850px]"
+            : "min-w-[700px]"
+        } table-fixed`}
+      >
         <colgroup>
           <col span={1} className="w-14" />
           <col span={1} className="w-[190px]" />
@@ -23,8 +34,8 @@ export default function FilteredConsultantList() {
             ?.bookings?.map((booking, index) => <col key={index} span={1} />)}
         </colgroup>
         <thead>
-          <tr>
-            <th colSpan={2}>
+          <tr className="sticky -top-6 bg-white z-10">
+            <th colSpan={2} className="pt-3 pl-2 -left-2 relative bg-white">
               <div className="flex flex-row gap-3 pb-4 items-center">
                 <p className="body-large-bold ">Konsulenter</p>
                 <p className="text-primary_default body-small-bold rounded-full bg-primary_l3 px-2 py-1">
@@ -35,7 +46,7 @@ export default function FilteredConsultantList() {
             {filteredConsultants.at(0)?.bookings?.map((booking) => (
               <th
                 key={booking.weekNumber}
-                className="m-2 px-2 py-1 gap-1 justify-items-end"
+                className="m-2 px-2 py-1 pt-3 gap-1 justify-items-end"
               >
                 {isCurrentWeek(booking.weekNumber, booking.year) ? (
                   <div className="flex flex-row gap-2 items-center justify-end">
