8214cadb1d509fe1a71d5e5dd531eed543303ae5
Magnus Gule <mg@variant.no>
2023-11-22 23:23:33 +0100

    Proper prop fix

--- frontend/src/hooks/useModal.ts
+++ frontend/src/hooks/useModal.ts
@@ -1,8 +1,8 @@
 import { useEffect, useRef } from "react";
 import { isClickOutsideElement } from "./useOutsideClick";
 
-export function useModal(options?: { closeOnBackdropClick?: boolean }) {
-  const { closeOnBackdropClick = true } = options || {};
+export function useModal(options?: { closeOnBackdropClick?: true }) {
+  const { closeOnBackdropClick } = options || {};
 
   const modalRef = useRef<HTMLDialogElement>(null);
   const modal = modalRef.current;
