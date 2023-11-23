9b835f483d95fa2646e894e7126d7e19d30679a6
Magnus Gule <mg@variant.no>
2023-11-23 11:13:53 +0100

    Adds EasyModal for basic usage + more features to PrimaryButton (#305)
    Co-authored-by: Mathilde Haukø Haugum <69193134+mathildehaugum@users.noreply.github.com>

--- frontend/src/components/RightCloseButton.tsx
+++ frontend/src/components/RightCloseButton.tsx
@@ -3,16 +3,12 @@ import { X } from "react-feather";
 
 export function RightCloseButton({ onClick }: { onClick: () => void }) {
   return (
-    <div className="relative h-8">
-      <div className="w-10 h-10 p-2 rounded-lg justify-center items-center gap-2 inline-flex">
-        <button
-          className="absolute top-0 right-0 w-8 h-8 flex justify-center items-center rounded bg-primary/0 hover:bg-primary/5"
-          onClick={onClick}
-        >
-          <X className="h-6 w-6 text-primary" />
-        </button>
-      </div>
-    </div>
+    <button
+      className="p-2 flex justify-center items-center rounded-lg hover:bg-primary/5 "
+      onClick={onClick}
+    >
+      <X className="h-6 w-6 text-primary" />
+    </button>
   );
 }
 
