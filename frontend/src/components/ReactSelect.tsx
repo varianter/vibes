b9235478e65f25c59b60740615705aee8b7ec399
Mathilde Haukø Haugum <mathilde_haugum@hotmail.com>
2023-11-28 10:14:34 +0100

    Update naming and add multiple option

--- frontend/src/components/ReactSelect.tsx
+++ frontend/src/components/ReactSelect.tsx
@@ -3,17 +3,20 @@ import Select from "react-select";
 
 export type SelectOption = { value: string; label: string };
 
-export default function TmpReactSelect({
+export default function ReactSelect({
   options,
   selectedValue,
   onChange,
+  isMultipleOptions = false,
 }: {
   options: SelectOption[];
   selectedValue: SelectOption | null;
   onChange: (arg: SelectOption) => void;
+  isMultipleOptions?: boolean;
 }) {
   return (
     <Select
+      isMulti={isMultipleOptions}
       options={options}
       value={selectedValue}
       onChange={(a) => a && onChange(a)}
