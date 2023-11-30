d60264ef6ecb40777a9126d4e95d1a32a6e11da4
Mathilde Haukø Haugum <mathilde_haugum@hotmail.com>
2023-11-30 11:00:39 +0100

    Remove unused button

--- frontend/src/components/WeekSelection.tsx
+++ frontend/src/components/WeekSelection.tsx
@@ -1,12 +1,12 @@
 "use client";
 import { ArrowLeft, ArrowRight } from "react-feather";
-import IconSecondaryButton from "./IconSecondaryButton";
 import { useSelectedWeek } from "@/hooks/staffing/useSelectedWeek";
 import DropDown from "./DropDown";
 import { useUrlRouteFilter } from "@/hooks/staffing/useUrlRouteFilter";
 import ActionButton from "./Buttons/ActionButton";
 import { LargeModal } from "./EasyModal/LargeModal";
 import ButtonExampleModal from "./EasyModal/ButtonExampleModal";
+import IconActionButton from "./Buttons/IconActionButton";
 
 export default function WeekSelection() {
   const weekSpanOptions = ["8 uker", "12 uker", "26 uker"];
@@ -30,12 +30,14 @@ export default function WeekSelection() {
       <ActionButton variant="secondary" onClick={resetSelectedWeek}>
         Nåværende uke
       </ActionButton>
-      <IconSecondaryButton
-        icon={<ArrowLeft size={24} />}
+      <IconActionButton
+        variant={"secondary"}
+        icon={<ArrowLeft />}
         onClick={decrementSelectedWeek}
       />
-      <IconSecondaryButton
-        icon={<ArrowRight size={24} />}
+      <IconActionButton
+        variant={"secondary"}
+        icon={<ArrowRight />}
         onClick={incrementSelectedWeek}
       />
     </div>
