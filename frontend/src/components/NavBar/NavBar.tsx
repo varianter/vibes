055c904d09b3a0ce7bf4fdf6291397f352ee6af5
Sigrid <55406589+sigtheidiot@users.noreply.github.com>
2023-12-01 08:07:13 +0100

    Add dropdown for organisation (#348)

--- frontend/src/components/NavBar/NavBar.tsx
+++ frontend/src/components/NavBar/NavBar.tsx
@@ -1,23 +1,19 @@
-import Image from "next/image";
 import NavBarLink from "./NavBarLink";
 import NavBarUserIcon from "./NavBarUserIcon";
+import { fetchWithToken } from "@/data/apiCallsWithToken";
+import { Organisation } from "@/types";
+import NavBarOrganisationDropdown from "./NavBarOrganisationDropdown";
 
-export default function NavBar() {
+export default async function NavBar() {
+  const orgs = (await fetchWithToken<Organisation[]>("organisations")) ?? [];
+
   return (
     <div className="bg-primary w-full flex flex-row justify-between header px-4">
       <div className="flex flex-row gap-8">
         <NavBarLink text="Bemanning" path={`bemanning`} />
       </div>
       <div className="flex flex-row gap-4 items-center">
-        <div className="border-r border-white/20 py-2">
-          <Image
-            className="variant-logo mr-4"
-            alt="Variant logo"
-            src="/images/variant-logo.svg"
-            width="65"
-            height="16"
-          />
-        </div>
+        <NavBarOrganisationDropdown organisations={orgs} />
         <NavBarUserIcon />
       </div>
     </div>
