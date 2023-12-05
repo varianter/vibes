37a647962218a1651ad62fcd1dd067e2e1cb5c5d
Magnus Gule <mg@variant.no>
2023-12-05 13:25:37 +0100

    Removing unused ActionButton on EasyModal. Need more control

--- frontend/src/components/Staffing/AddEngagementForm.tsx
+++ frontend/src/components/Staffing/AddEngagementForm.tsx
@@ -11,6 +11,13 @@ import ReactSelect, { SelectOption } from "@/components/ReactSelect";
 import { MultiValue } from "react-select";
 import EasyModal from "@/components/Modals/EasyModal";
 import { AddEngagementHoursModal } from "@/components/Staffing/AddEngagementHoursModal";
+import {
+  EngagementWriteModel,
+  ProjectState,
+  ProjectWithCustomerModel,
+} from "@/types";
+import { usePathname } from "next/navigation";
+import ActionButton from "../Buttons/ActionButton";
 
 export function AddEngagementForm({
   closeEngagementModal,
@@ -22,6 +29,9 @@ export function AddEngagementForm({
   const { openModal, modalRef } = useModal({
     closeOnBackdropClick: false,
   });
+
+  const organisationName = usePathname().split("/")[1];
+
   const { customers, consultants } = useContext(FilteredContext);
   // State for select components
   const [selectedCustomer, setSelectedCustomer] = useState<SelectOption | null>(
@@ -62,7 +72,7 @@ export function AddEngagementForm({
     ) ?? [];
 
   // State for radio button group
-  const [radioValue, setRadioValue] = useState("Tilbud");
+  const [radioValue, setRadioValue] = useState(ProjectState.Offer);
 
   // State for toggle
   const [isFakturerbar, setIsFakturerbar] = useState(false);
@@ -79,7 +89,7 @@ export function AddEngagementForm({
 
   // Handler for radio button group
   function handleRadioChange(event: ChangeEvent<HTMLInputElement>) {
-    setRadioValue(event.target.value);
+    setRadioValue(event.target.value as ProjectState);
   }
 
   // Handler for toggle
@@ -87,11 +97,41 @@ export function AddEngagementForm({
     setIsFakturerbar(!isFakturerbar);
   }
 
+  async function submitAddEngagementForm(body: EngagementWriteModel) {
+    const url = `/${organisationName}/bemanning/api/engagements`;
+
+    try {
+      const data = await fetch(url, {
+        method: "put",
+        body: JSON.stringify({
+          ...body,
+        }),
+      });
+      return (await data.json()) as ProjectWithCustomerModel;
+    } catch (e) {
+      console.error("Error updating staffing", e);
+    }
+  }
+
   // Handler for form submission
-  function handleSubmit(event: FormEvent<HTMLFormElement>) {
+  async function handleSubmit(event: FormEvent<HTMLFormElement>) {
     // Add your submission logic here
     console.log(event);
     console.log("Form submitted!");
+
+    const body: EngagementWriteModel = {
+      consultantIds: selectedConsultants?.map((c) => Number(c.value)), // Solid existing
+      customerId: Number(selectedCustomer?.value),
+      customerName: selectedCustomer?.label,
+      engagementId: Number(selectedEngagement?.value),
+      projectName: selectedEngagement?.label,
+      bookingType: radioValue,
+      isBillable: isFakturerbar,
+    };
+
+    const result = await submitAddEngagementForm(body);
+
+    console.log(result);
 
     //TODO: Need to close the add engagement modal before opening the large modal
     event.preventDefault();
@@ -144,8 +184,8 @@ export function AddEngagementForm({
                 <label className="flex gap-2 normal items-center">
                   <input
                     type="radio"
-                    value="Tilbud"
-                    checked={radioValue === "Tilbud"}
+                    value={ProjectState.Offer}
+                    checked={radioValue === ProjectState.Offer}
                     onChange={handleRadioChange}
                   />
                   Tilbud
@@ -153,8 +193,8 @@ export function AddEngagementForm({
                 <label className="flex gap-2 normal items-center">
                   <input
                     type="radio"
-                    value="Ordre"
-                    checked={radioValue === "Ordre"}
+                    value={ProjectState.Order}
+                    checked={radioValue === ProjectState.Order}
                     onChange={handleRadioChange}
                   />
                   Ordre
@@ -178,8 +218,14 @@ export function AddEngagementForm({
               </label>
             </div>
 
-            {/* Submit Button */}
-            <button type="submit">Submit</button>
+            <ActionButton
+              variant="primary"
+              type="submit"
+              fullWidth
+              onClick={() => {}}
+            >
+              Legg til engasjement
+            </ActionButton>
           </form>
         </div>
       </EasyModal>
