ce496f679b27aff7c842ad85dbea389e15cd784a
Magnus Gule <mg@variant.no>
2023-12-07 07:41:15 +0100

    Added IsClearable prop to ReactSelect

--- frontend/src/components/ReactSelect.tsx
+++ frontend/src/components/ReactSelect.tsx
@@ -10,6 +10,7 @@ export default function ReactSelect({
   onSingleOptionChange,
   onMultipleOptionsChange,
   isMultipleOptions = false,
+  isClearable = false,
 }: {
   options: SelectOption[];
   selectedSingleOptionValue?: SelectOption | null;
@@ -17,11 +18,13 @@ export default function ReactSelect({
   onSingleOptionChange?: (arg: SelectOption) => void;
   onMultipleOptionsChange?: (arg: MultiValue<SelectOption>) => void;
   isMultipleOptions?: boolean;
+  isClearable?: boolean;
 }) {
   return (
     <Select
       isMulti={isMultipleOptions}
       options={options}
+      isClearable={isClearable}
       value={
         isMultipleOptions
           ? selectedMultipleOptionsValue
