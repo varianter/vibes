164b833c68d3fbc31372156373927047ae66d11a
Jonas Bjøralt <jbb@variant.no>
2023-12-08 12:47:26 +0100

    Add migrations missing from prev PR

---
+++ backend/Database/Migrations/20231208112712_RenameProjectToEngagement.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameProjectToEngagement : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Staffing_Project_ProjectId",
+                table: "Staffing");
+
+            migrationBuilder.RenameColumn(
+                name: "ProjectId",
+                table: "Staffing",
+                newName: "EngagementId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Staffing_Project_EngagementId",
+                table: "Staffing",
+                column: "EngagementId",
+                principalTable: "Project",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Staffing_Project_EngagementId",
+                table: "Staffing");
+
+            migrationBuilder.RenameColumn(
+                name: "EngagementId",
+                table: "Staffing",
+                newName: "ProjectId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Staffing_Project_ProjectId",
+                table: "Staffing",
+                column: "ProjectId",
+                principalTable: "Project",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
