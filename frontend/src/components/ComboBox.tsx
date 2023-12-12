697f365d4cd1cdd1b0e19160684402ccd18866a2
Mathilde Haukø Haugum <mathilde_haugum@hotmail.com>
2023-12-12 10:30:38 +0100

    Remove creatable feature for consultant select

--- frontend/src/components/ComboBox.tsx
+++ frontend/src/components/ComboBox.tsx
@@ -1,6 +1,6 @@
 import { FilteredContext } from "@/hooks/ConsultantFilterProvider";
 import { useContext } from "react";
-import { MultiValue, createFilter } from "react-select";
+import Select, { MultiValue, createFilter } from "react-select";
 import CreatableSelect from "react-select/creatable";
 
 export type SelectOption = { value: string | number; label: string };
@@ -15,6 +15,7 @@ export default function ComboBox({
   isDisabled = false,
   placeHolderText = "Velg...",
   isClearable = false,
+  isCreatable = false,
 }: {
   options: SelectOption[];
   selectedSingleOptionValue?: SelectOption | null;
@@ -25,51 +26,95 @@ export default function ComboBox({
   isDisabled?: boolean;
   placeHolderText?: string;
   isClearable?: boolean;
+  isCreatable?: boolean;
 }) {
   const { setCloseModalOnBackdropClick } = useContext(FilteredContext);
   const customFilter = createFilter({
     matchFrom: "start",
   });
 
-  return (
-    <CreatableSelect
-      onFocus={() => setCloseModalOnBackdropClick(false)}
-      onBlur={() => setCloseModalOnBackdropClick(true)}
-      placeholder={placeHolderText}
-      isMulti={isMultipleOptions}
-      options={options}
-      isDisabled={isDisabled}
-      isClearable={isClearable}
-      filterOption={customFilter}
-      formatCreateLabel={(inputText) => `Legg til "${inputText}"`}
-      value={
-        isMultipleOptions
-          ? selectedMultipleOptionsValue
-          : selectedSingleOptionValue
-      }
-      onChange={(a) => {
-        a && isMultipleOptions
-          ? onMultipleOptionsChange?.(a as MultiValue<SelectOption>)
-          : onSingleOptionChange?.(a as SelectOption);
-      }}
-      styles={{
-        valueContainer: (base) => ({
-          ...base,
-          overflowX: "scroll",
-          flexWrap: "unset",
-          "::-webkit-scrollbar": {
-            display: "none",
-          },
-        }),
-        multiValue: (base) => ({
-          ...base,
-          flex:
-            selectedMultipleOptionsValue?.length &&
-            selectedMultipleOptionsValue?.length >= 2
-              ? "1 0 auto"
-              : "",
-        }),
-      }}
-    />
-  );
+  if (!isCreatable) {
+    return (
+      <Select
+        onFocus={() => setCloseModalOnBackdropClick(false)}
+        onBlur={() => setCloseModalOnBackdropClick(true)}
+        placeholder={placeHolderText}
+        isMulti={isMultipleOptions}
+        options={options}
+        isDisabled={isDisabled}
+        isClearable={isClearable}
+        filterOption={customFilter}
+        value={
+          isMultipleOptions
+            ? selectedMultipleOptionsValue
+            : selectedSingleOptionValue
+        }
+        onChange={(a) => {
+          a && isMultipleOptions
+            ? onMultipleOptionsChange?.(a as MultiValue<SelectOption>)
+            : onSingleOptionChange?.(a as SelectOption);
+        }}
+        styles={{
+          valueContainer: (base) => ({
+            ...base,
+            overflowX: "scroll",
+            flexWrap: "unset",
+            "::-webkit-scrollbar": {
+              display: "none",
+            },
+          }),
+          multiValue: (base) => ({
+            ...base,
+            flex:
+              selectedMultipleOptionsValue?.length &&
+              selectedMultipleOptionsValue?.length >= 2
+                ? "1 0 auto"
+                : "",
+          }),
+        }}
+      />
+    );
+  } else {
+    return (
+      <CreatableSelect
+        onFocus={() => setCloseModalOnBackdropClick(false)}
+        onBlur={() => setCloseModalOnBackdropClick(true)}
+        placeholder={placeHolderText}
+        isMulti={isMultipleOptions}
+        options={options}
+        isDisabled={isDisabled}
+        isClearable={isClearable}
+        filterOption={customFilter}
+        formatCreateLabel={(inputText) => `Legg til "${inputText}"`}
+        value={
+          isMultipleOptions
+            ? selectedMultipleOptionsValue
+            : selectedSingleOptionValue
+        }
+        onChange={(a) => {
+          a && isMultipleOptions
+            ? onMultipleOptionsChange?.(a as MultiValue<SelectOption>)
+            : onSingleOptionChange?.(a as SelectOption);
+        }}
+        styles={{
+          valueContainer: (base) => ({
+            ...base,
+            overflowX: "scroll",
+            flexWrap: "unset",
+            "::-webkit-scrollbar": {
+              display: "none",
+            },
+          }),
+          multiValue: (base) => ({
+            ...base,
+            flex:
+              selectedMultipleOptionsValue?.length &&
+              selectedMultipleOptionsValue?.length >= 2
+                ? "1 0 auto"
+                : "",
+          }),
+        }}
+      />
+    );
+  }
 }
