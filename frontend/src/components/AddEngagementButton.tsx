21daedfbd4fcdec1ed385555bb4d0486699ecee0
Sigrid Elnan <sge@variant.no>
2023-12-21 13:42:38 +0100

    Things aren´t really working

--- frontend/src/components/AddEngagementButton.tsx
+++ frontend/src/components/AddEngagementButton.tsx
@@ -2,9 +2,8 @@
 import { useModal } from "@/hooks/useModal";
 import { AddEngagementForm } from "./Staffing/AddEngagementForm";
 import { AddEngagementHoursModal } from "./Staffing/AddEngagementHoursModal/AddEngagementHoursModal";
-import { useContext, useState } from "react";
+import { useState } from "react";
 import { ConsultantReadModel, ProjectWithCustomerModel } from "@/api-types";
-import { FilteredContext } from "@/hooks/ConsultantFilterProvider";
 import { Plus } from "react-feather";
 import ActionButton from "./Buttons/ActionButton";
 
@@ -25,14 +24,10 @@ export default function AddEngagementButton() {
     closeOnBackdropClick: false,
   });
 
-  const [isAddStaffingHovered, setIsAddStaffingHovered] = useState(false);
-
   const [selectedConsultants, setSelectedConsultants] =
     useState<ConsultantReadModel[]>();
   const [selectedProject, setSelectedProject] =
     useState<ProjectWithCustomerModel>();
-
-  const { setIsDisabledHotkeys } = useContext(FilteredContext);
 
   function handleNewEngagement(
     project: ProjectWithCustomerModel,
@@ -47,10 +42,7 @@ export default function AddEngagementButton() {
   return (
     <>
       <ActionButton
-        onClick={() => {
-          openAddEngagementModal();
-          setIsDisabledHotkeys(true);
-        }}
+        onClick={openAddEngagementModal}
         className="flex flex-row items-center gap-2"
         variant={"primary"}
         iconLeft={<Plus />}
@@ -60,7 +52,6 @@ export default function AddEngagementButton() {
       <AddEngagementForm
         closeEngagementModal={handleNewEngagement}
         easyModalRef={addEngagementModalRef}
-        consultant={undefined}
       />
 
       <AddEngagementHoursModal
