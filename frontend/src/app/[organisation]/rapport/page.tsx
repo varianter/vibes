5174d57768511da844d85374bf05ba8d6bea8363
md <md@variant.no>
2024-03-05 09:45:42 +0100

    created reportPage setup

---
+++ frontend/src/app/[organisation]/rapport/page.tsx
@@ -0,0 +1,59 @@
+import {
+  CompetenceReadModel,
+  ConsultantReadModel,
+  DepartmentReadModel,
+  EngagementPerCustomerReadModel,
+} from "@/api-types";
+import { ConsultantFilterProvider } from "@/hooks/ConsultantFilterProvider";
+import { parseYearWeekFromUrlString } from "@/data/urlUtils";
+import React from "react";
+import { fetchWithToken } from "@/data/apiCallsWithToken";
+import { ReportContent } from "@/components/staffingReports/ReportContent";
+
+export default async function Rapport({
+  params,
+  searchParams,
+}: {
+  params: { organisation: string };
+  searchParams: { selectedWeek?: string; weekSpan?: string };
+}) {
+  const selectedWeek = parseYearWeekFromUrlString(
+    searchParams.selectedWeek || undefined,
+  );
+  const weekSpan = searchParams.weekSpan || undefined;
+
+  const consultants =
+    (await fetchWithToken<ConsultantReadModel[]>(
+      `${params.organisation}/staffings${
+        selectedWeek
+          ? `?Year=${selectedWeek.year}&Week=${selectedWeek.weekNumber}`
+          : ""
+      }${weekSpan ? `${selectedWeek ? "&" : "?"}WeekSpan=${weekSpan}` : ""}`,
+    )) ?? [];
+
+  const departments =
+    (await fetchWithToken<DepartmentReadModel[]>(
+      `organisations/${params.organisation}/departments`,
+    )) ?? [];
+
+  const competences =
+    (await fetchWithToken<CompetenceReadModel[]>(`competences`)) ?? [];
+
+  const customers =
+    (await fetchWithToken<EngagementPerCustomerReadModel[]>(
+      `${params.organisation}/projects`,
+    )) ?? [];
+
+  console.log(consultants);
+
+  return (
+    <ConsultantFilterProvider
+      consultants={consultants}
+      departments={departments}
+      competences={competences}
+      customers={customers}
+    >
+      <ReportContent />
+    </ConsultantFilterProvider>
+  );
+}
---
+++ frontend/src/components/staffingReports/ReportContent.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import StaffingSidebar from "@/components/StaffingSidebar";
+import { useState } from "react";
+import IconActionButton from "@/components/Buttons/IconActionButton";
+import { Filter } from "react-feather";
+import ActiveFilters from "@/components/ActiveFilters";
+import WeekSelection from "@/components/WeekSelection";
+
+export function ReportContent() {
+  const [isSideBarOpen, setIsSidebarOpen] = useState<boolean>(false);
+
+  return (
+    <>
+      <StaffingSidebar
+        isSidebarOpen={isSideBarOpen}
+        closeSidebar={() => setIsSidebarOpen(false)}
+      />
+
+      <div className="main p-4 pt-5 w-full flex flex-col gap-8">
+        <h1>Rapporter</h1>
+        <div className="flex flex-row justify-between items-center pt-[12px]">
+          <div className="flex flex-row items-center gap-3">
+            <IconActionButton
+              variant={"secondary"}
+              icon={<Filter />}
+              onClick={() => setIsSidebarOpen((wasOpen) => !wasOpen)}
+            />
+            <ActiveFilters />
+          </div>
+
+          <WeekSelection />
+        </div>
+        {/* INNHOLD HER */}
+        Innhold her
+      </div>
+    </>
+  );
+}
