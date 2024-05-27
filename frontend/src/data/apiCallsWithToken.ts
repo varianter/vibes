03ea8e91cd6b92774a106d4f4a6a41e3b1e987a2
olavbm <olavmarkussen@hotmail.no>
2024-05-27 15:32:06 +0200

    Use imageThumbUrl for consultant images

--- frontend/src/data/apiCallsWithToken.ts
+++ frontend/src/data/apiCallsWithToken.ts
@@ -112,7 +112,7 @@ export async function callEmployee(path: string) {
       );
       return {
         ...consultant,
-        imageUrl: imageCons?.imageUrl,
+        imageThumbUrl: imageCons?.imageThumbUrl,
       };
     });
 
