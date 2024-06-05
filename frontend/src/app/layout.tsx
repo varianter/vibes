f355c9795afe915d09acf52f4ea383fbeb42e13c
Jon <jon.heggstad@gmail.com>
2024-06-05 09:55:40 +0200

    chore: remove vercel favicon (#477)

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -4,6 +4,13 @@ import NextTopLoader from "nextjs-toploader";
 
 import "./globals.css";
 import NavBar from "@/components/NavBar/NavBar";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  icons: {
+    icon: "/favicon.png",
+  },
+};
 
 export default function RootLayout({
   children,
@@ -14,6 +21,7 @@ export default function RootLayout({
     <html>
       <Head>
         <title>Vibes Frontend</title>
+        <link rel="icon" href="/favicon.png" />
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width"
