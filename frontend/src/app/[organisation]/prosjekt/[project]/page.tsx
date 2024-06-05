35efdaa701929d5995e846c49663a9cf68faa12e
TrymVei <tve@variant.no>
2024-06-05 10:32:41 +0200

    feat: get project by url

--- frontend/src/app/[organisation]/prosjekt/[project]/page.tsx
+++ frontend/src/app/[organisation]/prosjekt/[project]/page.tsx
@@ -1,3 +1,20 @@
-export default async function Project() {
-  return <div>page</div>;
+import { ProjectWithCustomerModel } from "@/api-types";
+import { fetchWithToken } from "@/data/apiCallsWithToken";
+
+export default async function Project({
+  params,
+}: {
+  params: { organisation: string; project: string };
+}) {
+  const project =
+    (await fetchWithToken<ProjectWithCustomerModel>(
+      `${params.organisation}/projects/get/${params.project}`,
+    )) ?? undefined;
+
+  return (
+    <div>
+      <h1>{project?.projectName}</h1>
+      <h2>{project?.customerName}</h2>
+    </div>
+  );
 }
