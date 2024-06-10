29869f8adc1cde5cd3975bdcdb275997726e6998
Jon-Inge Heggstad <jon.heggstad@gmail.com>
2024-06-10 15:31:43 +0200

    feat: create forecast table
    Co-authored-by: idamand <ima@variant.no>

---
+++ backend/Core/Month/Month.cs
@@ -0,0 +1,50 @@
+namespace Core.DomainModels;
+
+public class Month
+{
+    public readonly int MonthNumber;
+
+    public readonly int Year;
+
+    public Month(int year, int monthNumber)
+    {
+        Year = year;
+        MonthNumber = monthNumber;
+    }
+
+    public Month(int monthAsInt)
+    {
+        Year = monthAsInt / 100;
+        MonthNumber = monthAsInt % 100;
+    }
+
+    /// <summary>
+    ///     Returns a string in the format yyyymm where y is year and m is month
+    ///     Example: 202401 or 202410
+    /// </summary>
+    public override string ToString()
+    {
+        return $"{ToSortableInt()}";
+    }
+
+    /// <summary>
+    ///     Returns an int in the format yyyymm where y is year and m is month
+    ///     Example: 202401 or 202410
+    /// </summary>
+    public int ToSortableInt()
+    {
+        return Year * 100 + MonthNumber;
+    }
+
+    // TODO: do we need this?
+    public Week GetWeekNumberOfFirstDayInMonth()
+    {
+        return Week.FromDateTime(new DateTime(Year, MonthNumber, 1));
+    }
+
+    // TODO: do we need this?
+    public Week GetWeekNumberOfLastDayInMonth()
+    {
+        return Week.FromDateTime(new DateTime(Year, MonthNumber, DateTime.DaysInMonth(Year, MonthNumber)));
+    }
+}
