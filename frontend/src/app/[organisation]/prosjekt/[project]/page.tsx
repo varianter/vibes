c5e57306033e9e1c8ee40627fffc4705c39c5773
TrymVei <tve@variant.no>
2024-06-12 14:01:06 +0200

    feat: added context to project page with consultant

--- frontend/src/app/[organisation]/prosjekt/[project]/page.tsx
+++ frontend/src/app/[organisation]/prosjekt/[project]/page.tsx
@@ -1,21 +1,46 @@
 import { EditEngagementHour } from "@/components/Staffing/EditEngagementHourModal/EditEngagementHour";
-import { fetchWithToken } from "@/data/apiCallsWithToken";
+import {
+  fetchEmployeesWithImageAndToken,
+  fetchWithToken,
+} from "@/data/apiCallsWithToken";
 import { ProjectWithCustomerModel } from "@/api-types";
 import Sidebar from "./Sidebar";
+import { ConsultantFilterProvider } from "@/hooks/ConsultantFilterProvider";
+import { parseYearWeekFromUrlString } from "@/data/urlUtils";
 
 export default async function Project({
   params,
+  searchParams,
 }: {
   params: { organisation: string; project: string };
+  searchParams: { selectedWeek?: string; weekSpan?: string };
 }) {
   const project =
     (await fetchWithToken<ProjectWithCustomerModel>(
       `${params.organisation}/projects/get/${params.project}`,
     )) ?? undefined;
 
+  const selectedWeek = parseYearWeekFromUrlString(
+    searchParams.selectedWeek || undefined,
+  );
+  const weekSpan = searchParams.weekSpan || undefined;
+
+  const consultants =
+    (await fetchEmployeesWithImageAndToken(
+      `${params.organisation}/staffings${selectedWeek
+        ? `?Year=${selectedWeek.year}&Week=${selectedWeek.weekNumber}`
+        : ""
+      }${weekSpan ? `${selectedWeek ? "&" : "?"}WeekSpan=${weekSpan}` : ""}`,
+    )) ?? [];
+
   if (project) {
     return (
-      <>
+      <ConsultantFilterProvider
+        consultants={consultants}
+        departments={[]}
+        competences={[]}
+        customers={[]}
+      >
         <Sidebar project={project} />
         <div className="main p-4 pt-5 w-full flex flex-col gap-8">
           <div className="flex flex-col gap-2">
@@ -25,7 +50,7 @@ export default async function Project({
 
           <EditEngagementHour project={project} />
         </div>
-      </>
+      </ConsultantFilterProvider>
     );
   } else {
     return <h1>Fant ikke prosjektet</h1>;
