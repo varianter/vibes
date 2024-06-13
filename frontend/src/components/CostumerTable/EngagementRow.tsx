8cfc04da48a71dc6597ca6acc7ec1697910fd8d1
olavbm <olavmarkussen@hotmail.no>
2024-06-13 09:15:10 +0200

    Feat/experience filter (#493)
    Co-authored-by: Jon <jon.heggstad@gmail.com>
    Co-authored-by: Ida Marie Andreassen <ima@variant.no>

--- frontend/src/components/CostumerTable/EngagementRow.tsx
+++ frontend/src/components/CostumerTable/EngagementRow.tsx
@@ -69,6 +69,8 @@ export default function EngagementRows({
         yearFilter: [],
         consultants: selectedConsultants,
         availabilityFilterOn: false,
+        activeExperienceFrom: activeFilters.experienceFromFilter,
+        activeExperienceTo: activeFilters.experienceToFilter,
       }),
     );
   }, [selectedConsultants, activeFilters.departmentFilter]);
