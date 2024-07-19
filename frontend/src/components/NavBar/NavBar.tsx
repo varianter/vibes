0b8acf43421c3e27f7c145a0c3e10fb8bc011176
Sigrid Elnan <sge@variant.no>
2024-07-19 14:43:32 +0200

    Make the navbar usable on mobile

--- frontend/src/components/NavBar/NavBar.tsx
+++ frontend/src/components/NavBar/NavBar.tsx
@@ -1,25 +1,48 @@
 import NavBarLink from "./NavBarLink";
-import NavBarUserIcon from "./NavBarUserIcon";
 import { fetchWithToken } from "@/data/apiCallsWithToken";
 import { OrganisationReadModel } from "@/api-types";
 import NavBarOrganisationDropdown from "./NavBarOrganisationDropdown";
+import NavBarMobile from "./NavBarMobile";
+import {
+  authOptions,
+  getCustomServerSession,
+} from "@/app/api/auth/[...nextauth]/route";
+import NavBarDropdown from "./NavBarDropdown";
+
+export const navBarLinks: { text: string; path: string }[] = [
+  { text: "Bemanning", path: "bemanning" },
+  { text: "Kunder", path: "kunder" },
+  { text: "Konsulenter", path: "konsulenter" },
+  { text: "Rapporter", path: "rapport" },
+];
 
 export default async function NavBar() {
   const orgs =
     (await fetchWithToken<OrganisationReadModel[]>("organisations")) ?? [];
 
+  const session =
+    !process.env.NEXT_PUBLIC_NO_AUTH &&
+    (await getCustomServerSession(authOptions));
+
+  const initial =
+    session && session.user && session.user.name ? session.user.name[0] : "N";
+
   return (
-    <div className="bg-primary w-full flex flex-row justify-between header px-4">
-      <div className="flex flex-row gap-8">
-        <NavBarLink text="Bemanning" path={`bemanning`} />
-        <NavBarLink text="Kunder" path={`kunder`} />
-        <NavBarLink text="Konsulenter" path={`konsulenter`} />
-        <NavBarLink text="Rapporter" path={`rapport`} />
+    <div className="bg-primary w-full header px-4">
+      <div className="hidden sm:flex flex-row justify-between">
+        <div className="flex flex-row gap-8">
+          {navBarLinks.map((link, index) => (
+            <NavBarLink key={index} text={link.text} path={link.path} />
+          ))}
+        </div>
+        <div className="flex flex-row gap-4 items-center">
+          <NavBarOrganisationDropdown organisations={orgs} />
+          <div className="w-[1px] h-8 bg-white/20" />
+          <NavBarDropdown initial={initial} />
+        </div>
       </div>
-      <div className="flex flex-row gap-4 items-center">
-        <NavBarOrganisationDropdown organisations={orgs} />
-        <div className="w-[1px] h-8 bg-white/20" />
-        <NavBarUserIcon />
+      <div className="flex sm:hidden">
+        <NavBarMobile orgs={orgs} initial={initial} />
       </div>
     </div>
   );
