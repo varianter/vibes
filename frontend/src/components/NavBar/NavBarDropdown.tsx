e4ff8ef8a82ba8ed2a4de7ecc02f5d4cad947b64
Truls Henrik <39614013+trulshj@users.noreply.github.com>
2024-12-20 13:40:28 +0100

    refactor: move "Ferie" into navbar (#555)

--- frontend/src/components/NavBar/NavBarDropdown.tsx
+++ frontend/src/components/NavBar/NavBarDropdown.tsx
@@ -18,43 +18,32 @@ export default function NavBarDropdown(props: { initial: string }) {
   });
 
   return (
-    <>
-      <div className="relative" ref={menuRef}>
+    <div className="relative" ref={menuRef}>
+      <button
+        className={`flex rounded-full border border-white/50 h-8 w-8 justify-center items-center  ${
+          isOpen ? "bg-white" : "hover:bg-white/10 hover:border-white"
+        }`}
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <p className={`large ${isOpen ? "text-primary" : "text-white"}`}>
+          {props.initial}
+        </p>
+      </button>
+      <div
+        className={`absolute right-0 top-[48px] z-50 rounded-lg text-primary bg-white flex flex-col w-[138px] shadow-xl p-1 ${
+          !isOpen && "hidden"
+        }`}
+      >
         <button
-          className={`flex rounded-full border border-white/50 h-8 w-8 justify-center items-center  ${
-            isOpen ? "bg-white" : "hover:bg-white/10 hover:border-white"
-          }`}
-          onClick={() => setIsOpen(!isOpen)}
+          className="hover:bg-primary/10 px-3 py-2 rounded flex flex-row gap-3 items-center "
+          onClick={() => signOut()}
         >
-          <p className={`large ${isOpen ? "text-primary" : "text-white"}`}>
-            {props.initial}
+          <LogOut className="w-6 h-6" />
+          <p className="h-6 flex items-center normal-semibold text-primary">
+            Logg ut
           </p>
         </button>
-        <div
-          className={`absolute right-0 top-[48px] z-50 rounded-lg text-primary bg-white flex flex-col w-[138px] shadow-xl p-1 ${
-            !isOpen && "hidden"
-          }`}
-        >
-          <Link
-            className="hover:bg-primary/10 px-3 py-2 rounded flex flex-row gap-3 items-center "
-            href={`/${orgUrl}/ferie`}
-          >
-            <Sun className="w-6 h-6" />
-            <p className="h-6 flex items-center normal-semibold text-primary">
-              Ferie
-            </p>
-          </Link>
-          <button
-            className="hover:bg-primary/10 px-3 py-2 rounded flex flex-row gap-3 items-center "
-            onClick={() => signOut()}
-          >
-            <LogOut className="w-6 h-6" />
-            <p className="h-6 flex items-center normal-semibold text-primary">
-              Logg ut
-            </p>
-          </button>
-        </div>
       </div>
-    </>
+    </div>
   );
 }
