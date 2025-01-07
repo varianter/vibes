3d650491099f8ddb11d3a186e79343ece930fd2e
Maria Katrin Bonde <mariabonde99@gmail.com>
2025-01-07 12:49:14 +0100

    added filters and title to forecast page

--- frontend/src/app/[organisation]/prognose/page.tsx
+++ frontend/src/app/[organisation]/prognose/page.tsx
@@ -1,10 +1,64 @@
+import {
+  CompetenceReadModel,
+  DepartmentReadModel,
+  EngagementPerCustomerReadModel,
+} from "@/api-types";
+import { ConsultantFilterProvider } from "@/hooks/ConsultantFilterProvider";
+import { parseYearWeekFromUrlString } from "@/data/urlUtils";
 import React from "react";
+import { StaffingContent } from "@/pagecontent/StaffingContent";
+import {
+  fetchEmployeesWithImageAndToken,
+  fetchWithToken,
+} from "@/data/apiCallsWithToken";
 import { Metadata } from "next";
-
+import { ForecastContent } from "@/pagecontent/ForecastContent";
 export const metadata: Metadata = {
   title: "Prognose | VIBES",
 };
 
-export default async function Prognose({}: {}) {
-  return <h1 className="main p-4 pt-5 w-full flex flex-col gap-8">Prognose</h1>;
+export default async function Prognose({
+  params,
+  searchParams,
+}: {
+  params: { organisation: string };
+  searchParams: { selectedWeek?: string; weekSpan?: string };
+}) {
+  const selectedWeek = parseYearWeekFromUrlString(
+    searchParams.selectedWeek || undefined,
+  );
+  const weekSpan = searchParams.weekSpan || undefined;
+
+  const consultants =
+    (await fetchEmployeesWithImageAndToken(
+      `${params.organisation}/staffings${
+        selectedWeek
+          ? `?Year=${selectedWeek.year}&Week=${selectedWeek.weekNumber}`
+          : ""
+      }${weekSpan ? `${selectedWeek ? "&" : "?"}WeekSpan=${weekSpan}` : ""}`,
+    )) ?? [];
+
+  const departments =
+    (await fetchWithToken<DepartmentReadModel[]>(
+      `organisations/${params.organisation}/departments`,
+    )) ?? [];
+
+  const competences =
+    (await fetchWithToken<CompetenceReadModel[]>(`competences`)) ?? [];
+
+  const customers =
+    (await fetchWithToken<EngagementPerCustomerReadModel[]>(
+      `${params.organisation}/projects`,
+    )) ?? [];
+
+  return (
+    <ConsultantFilterProvider
+      consultants={consultants}
+      departments={departments}
+      competences={competences}
+      customers={customers}
+    >
+      <ForecastContent />
+    </ConsultantFilterProvider>
+  );
 }
