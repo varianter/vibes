584ccf6e213eb9c7cea48902ee326377f9f6fa10
Maria Katrin Bonde <78419778+MariaBonde@users.noreply.github.com>
2025-01-14 14:11:15 +0100

    580 add number of hours to each month (#582)

---
+++ frontend/src/components/Forecast/BusinessHoursPerMonth.ts
@@ -0,0 +1,38 @@
+function getBusinessHoursPerMonth(
+  month: number,
+  year: number,
+  numWorkHours: number,
+  publicHolidayDays: string[],
+) {
+  const firstDay = new Date(year, month, 1);
+  const lastDay = new Date(year, month + 1, 0);
+  const hoursInDay = numWorkHours / 5;
+  var businessDaysCount = 0;
+  var currentDate = firstDay;
+  while (currentDate <= lastDay) {
+    var dayOfWeek = currentDate.getDay();
+    if (!(dayOfWeek == 6 || dayOfWeek == 0)) {
+      if (!isPublicHoliday(currentDate, publicHolidayDays)) {
+        businessDaysCount++;
+      }
+    }
+    currentDate.setDate(currentDate.getDate() + 1);
+  }
+  return businessDaysCount * hoursInDay;
+}
+
+function isPublicHoliday(day: Date, publicHolidayDays: string[]) {
+  const month = day.getMonth();
+  const dayString = `${day.getFullYear().toString()}-${
+    month > 8 ? (month + 1).toString() : "0" + (month + 1).toString()
+  }-${
+    day.getDate() > 9
+      ? day.getDate().toString()
+      : "0" + day.getDate().toString()
+  }`;
+
+  const isHoliday = publicHolidayDays.includes(dayString);
+  return isHoliday;
+}
+
+export { getBusinessHoursPerMonth };
