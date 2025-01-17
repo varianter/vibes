8f69e24652524976c2da14779884c605d0272824
Ida Marie Andreassen <ima@variant.no>
2025-01-17 10:49:54 +0100

    Remove forecastcell as component and implement it directly in MonthCell

--- frontend/src/components/Forecast/MonthCell.tsx
+++ frontend/src/components/Forecast/MonthCell.tsx
@@ -10,10 +10,9 @@ import {
   Sun,
 } from "react-feather";
 import { getInfopillVariantByColumnCount } from "@/components/Staffing/helpers/utils";
-import React from "react";
+import React, { useState } from "react";
 import { has } from "lodash";
 import { HoveredMonth } from "./HoveredMonth";
-import ForecastCell from "./ForecastCell";
 import RenderInfoPills from "../Staffing/RenderInfoPills";
 
 export function MonthCell(props: {
@@ -44,15 +43,26 @@ export function MonthCell(props: {
   } = props;
 
   const uneditable = forecastValue === 100;
+
+  const [forecast, setForecast] = useState(forecastValue);
+  const [isInputFocused, setIsInputFocused] = useState(false);
+  const [isChangingHours, setIsChangingHours] = useState(false);
+
   return (
     <td
       key={month}
       className={`h-[52px] ${isLastCol ? "py-0.5 pl-0.5" : "p-0.5"}`}
     >
       <div
-        className={`flex bg-primary/[3%] flex-col gap-1 p-2 justify-end rounded w-full h-full relative border border-transparent hover:border-primary/30 hover:cursor-pointer `}
-        onMouseEnter={() => setHoveredRowWeek(month)}
-        onMouseLeave={() => setHoveredRowWeek(-1)}
+        className={`flex bg-primary/[3%] flex-col gap-1 p-2 justify-end rounded w-full h-full relative border border-transparent hover:border-primary/30 `}
+        onMouseEnter={() => {
+          setHoveredRowWeek(month);
+          setIsChangingHours(true);
+        }}
+        onMouseLeave={() => {
+          setHoveredRowWeek(-1);
+          setIsChangingHours(false);
+        }}
       >
         {hoveredRowWeek != -1 && hoveredRowWeek == month && (
           <HoveredMonth
@@ -69,18 +79,37 @@ export function MonthCell(props: {
             columnCount={columnCount}
           />
         )}
-        <p
-          className={`text-right flex justify-end gap-[0.05rem] ${
-            uneditable ? "text-primary/60" : "text-primary"
-          }`}
-        >
-          {/*  {checkIfNotStartedOrQuit(consultant, bookedHoursPerMonth, numWorkHours)
-            ? "-"
-            : bookedHoursPerMonth.bookingModel.totalBillable.toLocaleString(
-                "nb-No",
-              )} */}
-          <ForecastCell forecastValue={forecastValue} />
-        </p>
+
+        <div className={`flex flex-row justify-end gap-[0.05rem] `}>
+          <input
+            type="number"
+            min="0"
+            step={10}
+            value={`${forecast}`}
+            draggable={true}
+            disabled={forecastValue >= 100}
+            onChange={(e) => setForecast(Number(e.target.value))}
+            onFocus={(e) => {
+              e.target.select();
+              setIsInputFocused(true);
+            }}
+            onBlur={() => {
+              setIsInputFocused(false);
+            }}
+            className={`${
+              forecastValue == forecast ? "small" : "small-medium"
+            } rounded w-full bg-transparent focus:outline-none min-w-[24px] text-right ${
+              uneditable ? "text-primary/60" : "text-primary"
+            }`}
+          />
+          <span
+            className={`${
+              forecastValue == forecast ? "small" : "small-medium"
+            } ${uneditable ? "text-primary/60" : "text-primary"} `}
+          >
+            %
+          </span>
+        </div>
       </div>
     </td>
   );
