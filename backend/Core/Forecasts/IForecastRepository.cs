9b48bfd187112c3c40020e73805027123766d0c7
Astrid Eldhuset <ae@variant.no>
2025-01-24 13:51:17 +0100

    Removing for-now-unused repository method

--- backend/Core/Forecasts/IForecastRepository.cs
+++ backend/Core/Forecasts/IForecastRepository.cs
@@ -6,9 +6,4 @@ public interface IForecastRepository
 	/// Gets the forecasts registered on the consultants with the given consultantId
 	/// </summary>
 	Task<Dictionary<int, List<Forecast>>> GetForecastForConsultants(List<int> consultantIds, CancellationToken cancellationToken);
-
-	/// <summary>
-	/// Updates the manually adjusted forecast value for the given consultant and month
-	/// </summary>
-	Task UpdateForecastForConsultant(int consultantId, DateOnly month, int value, CancellationToken cancellationToken);
 }
--- backend/Infrastructure/Repositories/Forecasts/ForecastDbRepository.cs
+++ backend/Infrastructure/Repositories/Forecasts/ForecastDbRepository.cs
@@ -1,4 +1,3 @@
-using Core.Extensions;
 using Core.Forecasts;
 using Infrastructure.DatabaseContext;
 using Microsoft.EntityFrameworkCore;
@@ -15,22 +14,5 @@ public class ForecastDbRepository(ApplicationContext context) : IForecastReposit
 			.ToDictionaryAsync(f => f.Key, f => f.ToList(), cancellationToken);
 
 		return forecastsByConsultant;
-	}
-
-	public async Task UpdateForecastForConsultant(int consultantId, DateOnly month, int value, CancellationToken cancellationToken)
-	{
-		var firstDayOfMonth = month.FirstDayInMonth();
-
-		// TODO Forecast: Does this logic support both (A) creating/adding a non-existing object and (B) updating an existing object (found by the composite key)?
-		var forecast = new Forecast
-		{
-			ConsultantId = consultantId,
-			Month = firstDayOfMonth,
-			AdjustedValue = value,
-		};
-
-		context.Forecasts.Update(forecast);
-
-		await context.SaveChangesAsync(cancellationToken);
 	}
 }
