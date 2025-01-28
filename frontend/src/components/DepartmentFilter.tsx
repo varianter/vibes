9981c067b9ff4f9814c1ff49fe36f069ef65ce17
Maria Katrin Bonde <mariabonde99@gmail.com>
2025-01-28 14:53:31 +0100

    fixing repercussions from altering filter handling files

--- frontend/src/components/DepartmentFilter.tsx
+++ frontend/src/components/DepartmentFilter.tsx
@@ -2,6 +2,7 @@
 import { useDepartmentFilter } from "@/hooks/staffing/useDepartmentFilter";
 import FilterButton from "./Buttons/FilterButton";
 import { Context } from "react";
+import { DepartmentReadModel } from "@/api-types";
 
 export default function DepartmentFilter({
   context,
@@ -12,7 +13,7 @@ export default function DepartmentFilter({
     useDepartmentFilter(context);
 
   if (departments.length > 0) {
-    departments.sort((a, b) => {
+    departments.sort((a: DepartmentReadModel, b: DepartmentReadModel) => {
       if (!(a.hotkey || b.hotkey)) {
         return a.id.localeCompare(b.id);
       }
@@ -26,16 +27,18 @@ export default function DepartmentFilter({
       <div className="flex flex-col gap-2">
         <p className="small">Avdeling</p>
         <div className="flex flex-col gap-2 w-52">
-          {departments?.map((department, index) => (
-            <FilterButton
-              key={department.id}
-              label={department.name}
-              onClick={() => toggleDepartmentFilter(department)}
-              checked={filteredDepartments
-                .map((d) => d.id)
-                .includes(department.id)}
-            />
-          ))}
+          {departments?.map(
+            (department: DepartmentReadModel, index: number) => (
+              <FilterButton
+                key={department.id}
+                label={department.name}
+                onClick={() => toggleDepartmentFilter(department)}
+                checked={filteredDepartments
+                  .map((d) => d.id)
+                  .includes(department.id)}
+              />
+            ),
+          )}
         </div>
       </div>
     );
