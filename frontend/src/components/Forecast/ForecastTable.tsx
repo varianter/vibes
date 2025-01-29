4ed30c92779f4df4f0144db6987c39c29d54688b
Maria Katrin Bonde <mariabonde99@gmail.com>
2025-01-29 11:16:28 +0100

    errorhandling

--- frontend/src/components/Forecast/ForecastTable.tsx
+++ frontend/src/components/Forecast/ForecastTable.tsx
@@ -5,6 +5,7 @@ import { fetchPublicHolidays } from "@/hooks/fetchPublicHolidays";
 import { usePathname } from "next/navigation";
 import { getBusinessHoursPerMonth } from "./BusinessHoursPerMonth";
 import { useForecastFilter } from "@/hooks/ForecastFilter/useForecastFilter";
+import { ForecastForMonth } from "@/api-types";
 
 function isCurrentMonth(dateString: string) {
   const date = new Date(dateString);
@@ -46,13 +47,15 @@ export default function ForecastTable() {
     <table className={`table-fixed`}>
       <colgroup>
         <col span={1} className="w-[190px]" />
-        {filteredConsultants[0].forecasts.map((forecast) => (
-          <col
-            key={`${forecast.month}`}
-            span={1}
-            className={`w-[calc((1%/15)*100)]`}
-          />
-        ))}
+        {filteredConsultants[0]?.forecasts?.map(
+          (forecast: ForecastForMonth) => (
+            <col
+              key={`${forecast.month}`}
+              span={1}
+              className={`w-[calc((1%/15)*100)]`}
+            />
+          ),
+        )}
       </colgroup>
       <thead>
         <tr className="sticky -top-6 bg-white z-10">
@@ -64,12 +67,13 @@ export default function ForecastTable() {
               </p>
             </div>
           </th>
-          {filteredConsultants[0].forecasts.map((forecast) => (
-            <th key={"" + forecast.month} className=" px-2 py-1 pt-3 ">
-              <div className="flex flex-col gap-1">
-                {isCurrentMonth(forecast.month) ? (
-                  <div className="flex flex-row gap-2 items-center justify-end">
-                    {/* {booking.bookingModel.totalHolidayHours > 0 && (
+          {filteredConsultants[0]?.forecasts?.map(
+            (forecast: ForecastForMonth) => (
+              <th key={"" + forecast.month} className=" px-2 py-1 pt-3 ">
+                <div className="flex flex-col gap-1">
+                  {isCurrentMonth(forecast.month) ? (
+                    <div className="flex flex-row gap-2 items-center justify-end">
+                      {/* {booking.bookingModel.totalHolidayHours > 0 && (
                       <InfoPill
                         text={booking.bookingModel.totalHolidayHours.toLocaleString(
                           "nb-No",
@@ -83,19 +87,19 @@ export default function ForecastTable() {
                         variant={weekSpan < 24 ? "wide" : "medium"}
                       />
                     )} */}
-                    <div className="h-2 w-2 rounded-full bg-primary" />
+                      <div className="h-2 w-2 rounded-full bg-primary" />
 
-                    <p className="normal-medium text-right">
-                      {getShortenedMonthName(forecast.month)}
-                    </p>
-                  </div>
-                ) : (
-                  <div
-                    className={`flex justify-end
+                      <p className="normal-medium text-right">
+                        {getShortenedMonthName(forecast.month)}
+                      </p>
+                    </div>
+                  ) : (
+                    <div
+                      className={`flex justify-end
                          flex-row gap-2
                     `}
-                  >
-                    {/*  {booking.bookingModel.totalHolidayHours > 0 && (
+                    >
+                      {/*  {booking.bookingModel.totalHolidayHours > 0 && (
                       <InfoPill
                         text={booking.bookingModel.totalHolidayHours.toLocaleString(
                           "nb-No",
@@ -109,25 +113,26 @@ export default function ForecastTable() {
                         variant={weekSpan < 24 ? "wide" : "medium"}
                       />
                     )} */}
-                    <p className="normal text-right">
-                      {getShortenedMonthName(forecast.month)}
-                    </p>
-                  </div>
-                )}
-                <p className="flex justify-end xsmall">
-                  {publicHolidays.length > 0
-                    ? "" +
-                      getBusinessHoursPerMonth(
-                        forecast.month,
-                        numWorkHours,
-                        publicHolidays,
-                      ) +
-                      "t"
-                    : "\u00A0"}
-                </p>
-              </div>
-            </th>
-          ))}
+                      <p className="normal text-right">
+                        {getShortenedMonthName(forecast.month)}
+                      </p>
+                    </div>
+                  )}
+                  <p className="flex justify-end xsmall">
+                    {publicHolidays.length > 0
+                      ? "" +
+                        getBusinessHoursPerMonth(
+                          forecast.month,
+                          numWorkHours,
+                          publicHolidays,
+                        ) +
+                        "t"
+                      : "\u00A0"}
+                  </p>
+                </div>
+              </th>
+            ),
+          )}
         </tr>
       </thead>
       <tbody>
