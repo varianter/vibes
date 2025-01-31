88625d6d7247f4a6355a36d96d4a0d4ef62f4858
Truls Henrik Jakobsen <thj@variant.no>
2025-01-31 12:19:45 +0100

    feat: spec out forecast repository

--- backend/Core/Forecasts/Forecast.cs
+++ backend/Core/Forecasts/Forecast.cs
@@ -2,13 +2,13 @@ namespace Core.Forecasts;
 
 public class Forecast
 {
-	public required int ConsultantId { get; set; }
+    public required int ConsultantId { get; init; }
 
-	public required DateOnly Month { get; set; }
+    public required DateOnly Month { get; init; }
 
-	public int? AdjustedValue { get; set; }
+    public int? AdjustedValue { get; init; }
 
-	public ForecastKey ForecastKey => new(ConsultantId, Month);
+    public ForecastKey ForecastKey => new(ConsultantId, Month);
 }
 
 public record struct ForecastKey(int ConsultantId, DateOnly Month);
--- backend/Core/Forecasts/IForecastRepository.cs
+++ backend/Core/Forecasts/IForecastRepository.cs
@@ -3,7 +3,20 @@ namespace Core.Forecasts;
 public interface IForecastRepository
 {
 	/// <summary>
+	///     Gets the forecast with the given key
+	/// </summary>
+	/// <returns>A <see cref="Forecast" /> if it exists, else <pre>null</pre></returns>
+	public Task<Forecast?> GetForecast(ForecastKey forecastKey, CancellationToken cancellationToken);
+	
+	/// <summary>
 	/// Gets the forecasts registered on the consultants with the given consultantId
 	/// </summary>
-	Task<Dictionary<int, List<Forecast>>> GetForecastForConsultants(List<int> consultantIds, CancellationToken cancellationToken);
+	public Task<Dictionary<int, List<Forecast>>> GetForecastForConsultants(List<int> consultantIds,
+		CancellationToken cancellationToken);
+
+	public Task<Forecast> AddForecast(Forecast forecast, CancellationToken cancellationToken);
+
+	public Task<Forecast[]> AddForecastsRange(IEnumerable<Forecast> forecasts, CancellationToken cancellationToken);
+
+	public Task<Forecast?> UpdateForecast(Forecast forecast, CancellationToken cancellationToken);
 }
