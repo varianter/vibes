5a85b6b65af564a6c9b043b0bdef44153ebf306b
Truls Henrik Jakobsen <thj@variant.no>
2025-02-03 12:33:30 +0100

    fix: use FirstOrDefault and return null instead of throwing with Single

--- backend/Api/Program.cs
+++ backend/Api/Program.cs
@@ -1,5 +1,6 @@
 using System.Text.Json.Serialization;
 using Api.AppExtensions;
+using Api.Common;
 using Api.Options;
 using Infrastructure;
 using Infrastructure.Repositories;
@@ -16,6 +17,8 @@ builder.Services.AddAuthorization(opt => { opt.FallbackPolicy = opt.DefaultPolic
 
 builder.Services.AddMemoryCache();
 builder.AddRepositories();
+
+builder.Services.AddScoped<IStorageService, StorageService>();
 
 builder.Services.AddControllers()
     .AddJsonOptions(options => { options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()); });
