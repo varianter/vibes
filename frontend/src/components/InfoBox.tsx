d84690ab5bc708249edf3adfe36f891cf49ac32f
robines <espi.robin@gmail.com>
2025-02-24 13:05:26 +0100

    Add optional loading state to InfoBox

--- frontend/src/components/InfoBox.tsx
+++ frontend/src/components/InfoBox.tsx
@@ -1,14 +1,20 @@
-export default function InfoBox({
-  infoName,
-  infoValue,
-}: {
+import { Skeleton } from "@/components/Skeleton";
+
+type Props = {
   infoName: string;
   infoValue: string | undefined;
-}) {
+  isLoading?: boolean;
+};
+
+export default function InfoBox({ infoName, infoValue, isLoading }: Props) {
   return (
     <div className="bg-primary/5 rounded-xl p-3 flex flex-col gap-2">
       <p className="small text-black">{infoName}</p>
-      <p className="normal-semibold text-black">{infoValue}</p>
+      {isLoading || infoValue === undefined ? (
+        <Skeleton className="h-4 w-8" />
+      ) : (
+        <p className="normal-semibold text-black">{infoValue}</p>
+      )}
     </div>
   );
 }
