ddbe5919fe4aecd2a3d3b398e67c1e60c04cc3b3
robines <espi.robin@gmail.com>
2025-02-25 09:46:42 +0100

    prettier

--- frontend/src/query-client.tsx
+++ frontend/src/query-client.tsx
@@ -5,20 +5,23 @@ import {
   QueryClient,
   QueryClientProvider,
 } from "@tanstack/react-query";
-import {PropsWithChildren, useState} from "react";
+import { PropsWithChildren, useState } from "react";
 
 export function ReactQueryClientProvider(props: PropsWithChildren) {
-  const [queryClient] = useState(() => new QueryClient({
-    defaultOptions: {
-      queries: {
-        refetchOnWindowFocus: false,
-      },
-    },
-    queryCache: new QueryCache({
-      onError: (err) => {
-        console.error(err);
-      },
-    }),
-  }))
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            refetchOnWindowFocus: false,
+          },
+        },
+        queryCache: new QueryCache({
+          onError: (err) => {
+            console.error(err);
+          },
+        }),
+      }),
+  );
   return <QueryClientProvider client={queryClient} {...props} />;
 }
