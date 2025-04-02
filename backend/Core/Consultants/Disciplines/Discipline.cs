436822193816e6ad7f51ba7fa46ae9ce3ae668d7
Astrid Eldhuset <ae@variant.no>
2025-04-02 15:36:51 +0200

    Preventing serialization of the Consultants property on Discipline objects

--- backend/Core/Consultants/Disciplines/Discipline.cs
+++ backend/Core/Consultants/Disciplines/Discipline.cs
@@ -1,8 +1,10 @@
+using System.Text.Json.Serialization;
+
 namespace Core.Consultants.Disciplines;
 
 public class Discipline
 {
     public required string Id { get; init; }
     public required string Name { get; init; }
-    public List<Consultant> Consultants { get; init; } = [];
+    [JsonIgnore] public List<Consultant> Consultants { get; init; } = [];
 }
