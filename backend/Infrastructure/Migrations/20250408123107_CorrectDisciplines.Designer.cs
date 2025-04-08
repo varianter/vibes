1ae570931b8f182c598295b8ced28ecd0a6b4f9c
Astrid Eldhuset <ae@variant.no>
2025-04-08 15:10:13 +0200

    Correcting the Discipline (Faggruppe) values in the database seed (#690)

--- backend/Infrastructure/Migrations/20250408123107_CorrectDisciplines.Designer.cs
+++ backend/Infrastructure/Migrations/20250408123107_CorrectDisciplines.Designer.cs
@@ -4,6 +4,7 @@ using Infrastructure.DatabaseContext;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20250408123107_CorrectDisciplines")]
+    partial class CorrectDisciplines
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
