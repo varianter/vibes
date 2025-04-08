4cd8c826d226fb2021e28df0560695374815fcbb
Astrid Eldhuset <ae@variant.no>
2025-04-08 14:37:54 +0200

    Adding migration

---
+++ backend/Infrastructure/Migrations/20250408123107_CorrectDisciplines.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class CorrectDisciplines : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Disciplines",
+                keyColumn: "Id",
+                keyValue: "app-development");
+
+            migrationBuilder.DeleteData(
+                table: "Disciplines",
+                keyColumn: "Id",
+                keyValue: "counselling");
+
+            migrationBuilder.DeleteData(
+                table: "Disciplines",
+                keyColumn: "Id",
+                keyValue: "data-engineer");
+
+            migrationBuilder.UpdateData(
+                table: "Disciplines",
+                keyColumn: "Id",
+                keyValue: "jvm",
+                column: "Name",
+                value: "JVM");
+
+            migrationBuilder.UpdateData(
+                table: "Disciplines",
+                keyColumn: "Id",
+                keyValue: "ppp",
+                column: "Name",
+                value: "PPP-ledelse");
+
+            migrationBuilder.UpdateData(
+                table: "Disciplines",
+                keyColumn: "Id",
+                keyValue: "strategic-design",
+                column: "Name",
+                value: "Strategisk design");
+
+            migrationBuilder.UpdateData(
+                table: "Disciplines",
+                keyColumn: "Id",
+                keyValue: "ux-design",
+                column: "Name",
+                value: "UX-design");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Disciplines",
+                keyColumn: "Id",
+                keyValue: "jvm",
+                column: "Name",
+                value: "Java/JVM");
+
+            migrationBuilder.UpdateData(
+                table: "Disciplines",
+                keyColumn: "Id",
+                keyValue: "ppp",
+                column: "Name",
+                value: "Prosjekt/Produkt/Prosess");
+
+            migrationBuilder.UpdateData(
+                table: "Disciplines",
+                keyColumn: "Id",
+                keyValue: "strategic-design",
+                column: "Name",
+                value: "Strategisk Design");
+
+            migrationBuilder.UpdateData(
+                table: "Disciplines",
+                keyColumn: "Id",
+                keyValue: "ux-design",
+                column: "Name",
+                value: "UX Design");
+
+            migrationBuilder.InsertData(
+                table: "Disciplines",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { "app-development", "App-utvikling" },
+                    { "counselling", "Rådgivning" },
+                    { "data-engineer", "Data Engineer" }
+                });
+        }
+    }
+}
