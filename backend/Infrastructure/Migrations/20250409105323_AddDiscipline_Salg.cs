5e43c1e9872896f419ef2812d5d7d9fc618729ed
Astrid Eldhuset <ae@variant.no>
2025-04-09 12:58:56 +0200

    Adding migration

---
+++ backend/Infrastructure/Migrations/20250409105323_AddDiscipline_Salg.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDiscipline_Salg : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Disciplines",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { "sales", "Salg" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Disciplines",
+                keyColumn: "Id",
+                keyValue: "sales");
+        }
+    }
+}
